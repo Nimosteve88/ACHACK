1e15e95877005d525057f9ea9ca5b07973485b75
Divine Wodi <wodidivine03@gmail.com>
2023-11-18 22:50:59 +0000

    urls

--- AC_Hack/members/urls.py
+++ AC_Hack/members/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.members, name='members'),
-    path('topics/', views.topics, name='topics'),
+    path('quiz1/', views.topics, name='quiz1'),
     path('rewards/', views.rewards, name='rewards'),
+    path('quiz2/', views.topics, name='quiz2'),
+    path('facts/', views.topics, name='facts'),
 ]
