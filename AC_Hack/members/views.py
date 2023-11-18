aabe93ed708838471856948216cec8da95211e1f
JollofBoy <pkasaam@gmail.com>
2023-11-18 17:29:54 +0000

    JollofBoy: added a quick test to see template

--- AC_Hack/members/views.py
+++ AC_Hack/members/views.py
@@ -12,3 +12,19 @@ def members(request):
 def topics(request):
     template = loader.get_template('topics.html')
     return HttpResponse(template.render())
+
+def rewards(request):
+    # players is gonna be taken from a database of scores
+    # TODO define the players document
+    template = loader.get_template('rewards.html')
+    players = [Player()]
+    context = {'previous_score' : 1, 'final_score' : 2, 'players': players} # this has some dummy values atm to just see the result
+    return HttpResponse(template.render(context, request))
+
+
+# making a quick class for the player just to test
+class Player():
+    def __init__(self) -> None:
+        self.rank = 1
+        self.name = "Caspar"
+        self.score = 1
