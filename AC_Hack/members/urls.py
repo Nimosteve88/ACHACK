5e07e6bc05d2e84b0fb0d71a7931e69d176ee26d
Nimosteve88 <nimosteve2@gmail.com>
2023-11-18 22:47:23 +0000

    update

--- AC_Hack/members/urls.py
+++ AC_Hack/members/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.members, name='members'),
+    path('', views.members, name='home'),
     path('topics/', views.topics, name='topics'),
     path('rewards/', views.rewards, name='rewards'),
 ]
