66005b9cee643163a93b6f4a93a9a7c828f3e232
Nimosteve88 <nimosteve2@gmail.com>
2023-11-19 00:55:46 +0000

    update

--- AC_Hack/members/urls.py
+++ AC_Hack/members/urls.py
@@ -7,11 +7,11 @@ urlpatterns = [
     path('rewards/', views.rewards, name='rewards'),
     path('quiz2/', views.quiz2, name='quiz2'),
     path('facts/', views.facts, name='facts'),
-    # path('belongingToACommunity/', views.belongingToACommunity, name='belongingToACommunity'),
-    # path('energyAndSustainability/', views.energyAndSustainability, name='energyAndSustainability'),
-    # path('financialLiteracy/', views.financialLiteracy, name='financialLiteracy'),
-    # path('healthyRelationships/', views.healthyRelationships, name='healthyRelationships'),
-    # path('keepingFit/', views.keepingFit, name='keepingFit'),
-    # path('mentalHealthAndWellBeing/', views.mentalHealthAndWellBeing, name='mentalHealthAndWellBeing'),
+    path('belongingToACommunity/', views.belongingToACommunity, name='belongingToACommunity'),
+    path('energyAndSustainability/', views.energyAndSustainability, name='energyAndSustainability'),
+    path('financialLiteracy/', views.financialLiteracy, name='financialLiteracy'),
+    path('healthyRelationships/', views.healthyRelationships, name='healthyRelationships'),
+    path('keepingFit/', views.keepingFit, name='keepingFit'),
+    path('mentalHealthAndWellBeing/', views.mentalHealthAndWellBeing, name='mentalHealthAndWellBeing'),
 
 ]
